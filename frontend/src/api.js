62777ce513aba8a1a1069b13337346270247eebc
TymFly <ggit@keemail.me>
2026-06-13 11:20:16 +0200

    migrate auth handling to backend

--- frontend/src/api.js
+++ frontend/src/api.js
@@ -1,4 +1,13 @@
 const _KEY = import.meta.env.VITE_API_KEY ?? ''
 
-export const apiFetch = (url, opts = {}) =>
-  fetch(url, { ...opts, headers: { ...opts.headers, ...(_KEY && { 'X-API-Key': _KEY }) } })
+export const apiFetch = (url, opts = {}) => {
+  const token = sessionStorage.getItem('admin_token')
+  return fetch(url, {
+    ...opts,
+    headers: {
+      ...opts.headers,
+      ...(token && { Authorization: `Bearer ${token}` }),
+      ...(_KEY && { 'X-API-Key': _KEY }),
+    },
+  })
+}
